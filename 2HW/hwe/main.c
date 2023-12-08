c438ac4f09b1baa1a954c02617df9aafae22b338
Kemal Bidzhiev <kemal@Kemals-MacBook-Pro.local>
2023-12-08 19:32:10 +0100

    stdin out for primes

--- 2HW/hwe/main.c
+++ 2HW/hwe/main.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include"sieve.h"
 #include"hwe.h"
 #include"tests.c"
 
 
 int main(){
-  test();
+  //test();
+  unsigned n, p, size;
+  struct sieve_t sv;
+
+  if(1 != scanf("%u", &n)) abort();
+
+  size = sieve_size(n);
+  init_sieve(&sv, size);
+  fill_sieve(&sv);
+
+  p = nth_prime(&sv, n);
+  printf("%u-th prime is %u\n", n, p);
 
   return 0;
 }

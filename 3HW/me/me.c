aa188dc8392e3919824c187fa0a1adc997363ce1
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2024-02-09 20:38:54 +0100

    shorter version

--- 3HW/me/me.c
+++ 3HW/me/me.c
@@ -1,33 +1,25 @@
 
-//#include "me.h"
+#include "me.h"
 
 
-//int majority_element(const int *parr, int len);
+int majority_element(const int *parr, int len);
 
 
 int majority_element(const int *parr, int len) {
-  int i, counter = 0;
-  int elem = parr[0];
-  for(i = 1; i < len; ++i) {
+  int i,elem, counter = 0;
+  for(i = 0; i < len; ++i) {
     if(0 == counter) {
       elem = parr[i];
       ++counter;
       continue;
     }
-    if(parr[i] == elem) {
-      ++counter; 
-    } else {
-      --counter;
-    }
+    parr[i] == elem ? ++counter : --counter;
   }
   counter = 0;
-  for( i = 0; i < len; ++i) {
+  for( i = 0; i < len; ++i) 
     if(elem == parr[i])
         ++counter;
-  } 
-  if(counter > len/2 )
-    return elem;
-  
-  return -1;
+   
+  return counter > (len/2) ? elem : -1;
 }
 

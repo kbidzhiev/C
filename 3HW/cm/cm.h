30283feb70dfaf4e080efb91b1e7cacff68ad1f1
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2024-02-14 19:59:34 +0100

    test for integers

--- 3HW/cm/cm.h
+++ 3HW/cm/cm.h
@@ -4,7 +4,7 @@
 
 int compute_size(int *sizes, int nelts, int begin, int num);
 
-//typedef int (*xcmp_t)(void *lhs, int lsz, void *rhs, int rsz);
-//void xmsort(void *mem, int *sizes, int nelts, xcmp_t cmp);
+typedef int (*xcmp_t)(void *lhs, int lsz, void *rhs, int rsz);
+void xmsort(void *mem, int *sizes, int nelts, xcmp_t cmp);
 
 #endif
--- 3HW/cm/main.c
+++ 3HW/cm/main.c
@@ -6,6 +6,7 @@
 
 
 //void xmsort(void *mem, int *sizes, int nelts, xcmp_t cmp);
+//typedef int (*xcmp_t)(void *lhs, int lsz, void *rhs, int rsz);
 
 int sum_arr_elem(int *sizes, int nelts, int begin, int num);
 void test_sum_arr_elem(int k) {
@@ -22,8 +23,46 @@ void test_sum_arr_elem(int k) {
   printf("%d. test_sum_arr_elem - OK;\n", k);
 }
 
+int cmp(void *lhs, int lsz, void *rhs, int rsz) {
+  int l, r;
+  if (lsz != rsz) {
+    assert(0);
+  }
+  l = *(int *)lhs;
+  r = *(int *)rhs;
+  if (l < r) return -1;
+  if (l > r) return 1;
+  return 0;
+}
+
+void test_sorting(int k) {
+  int s = sizeof(int);
+  int arr[4] = {3, 2, 6, 1};
+  int sizes[4] = {s, s, s, s};
+  for(s = 0; s < 4; ++s) {
+    printf("%d ", arr[s]);
+  }
+  printf("\n");
+
+  xmsort(arr, sizes, 4, cmp);
+  
+  for(s = 0; s < 4; ++s) {
+    printf("%d ", arr[s]);
+  }
+  printf("\n");
+  assert(1 == arr[0]); 
+  assert(2 == arr[1]); 
+  assert(3 == arr[2]); 
+  assert(6 == arr[3]);
+
+  printf("%d. test_sorting - OK;\n", k);
+}
+
+
 void test_all(void) {
   test_sum_arr_elem(1);
+  test_sorting(2);
+  printf("All tests are OK\n");
 }
 
 

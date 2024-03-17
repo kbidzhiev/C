4836102be45723a033d0b146173b8eb2a2de4b38
ENTER YOUR NAME <Enter_your@email.com>
2024-03-17 20:21:26 +0100

    powNN

--- 3HW/px/px.c
+++ 3HW/px/px.c
@@ -14,7 +14,7 @@ void powNxN (unsigned (*A)[N], unsigned k, unsigned m) {
 }
 */
 
-typedef unsigned (*row_t)[N];
+typedef unsigned (row_t)[N];
 typedef row_t * mat_t;
 
 
@@ -24,89 +24,85 @@ mat_t Mcalloc() {
     printf("Fail to allocate N rows\n");
     abort();
   }
-  for(int i = 0; i < N; ++i) {
-    M[i] = calloc(1, sizeof(unsigned[N]));
-    if (NULL == M[i]) {
-      printf("Fail to allocate row %d, cleaning up\n", i);
-      while(--i > 0)
-        free(M[i]);
-      free(M); 
-      abort();
-    }
-  }
   return M;
 }
 
 void Mfree(mat_t M) {
-  for (int i = 0; i < N; ++i) {
-    free(M[i]);
-  }
   free(M);
 }
 void Mprintf(mat_t M) {
   for(int i = 0; i < N; ++i) {
-    for(int j = 0; j < N; ++j) {
-      printf("%u ", M[i][0][j]);
-    }
+    for(int j = 0; j < N; ++j)
+      printf("%u ", M[i][j]);
     printf("\n");
   }
 }
 
 mat_t Id() {
   mat_t M = Mcalloc();
-  for(int i = 0; i < N; ++i) {
-    for(int j = 0; j < N; ++j) {
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < N; ++j)
       if (i == j)
-        M[i][0][j] = 1;
-    }
-  }
+        M[i][j] = 1;
   return M;
 }
 
 void rem(mat_t M, unsigned mod) {
-  for(int i = 0; i < N; ++i) {
-    for(int j = 0; j < N; ++j) {
-      M[i][0][j] = M[i][0][j] % mod;
-    }
-  }
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < N; ++j)
+      M[i][j] = M[i][j] % mod;
 }
 
 mat_t matmul(mat_t A, mat_t B) {
   mat_t res = Mcalloc();
-  for(int i = 0; i < N; ++i) {
-    for(int j = 0; j < N; ++j) {
-      for(int k = 0; k < N; ++k) {
-        printf("%d %d %d\n", i , j, k);
-        res[i][0][j] += A[i][0][k] * B[k][0][j];
-      }
-    }
-  }
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < N; ++j)
+      for(int k = 0; k < N; ++k)
+        res[i][j] += A[i][k] * B[k][j];
   return res;
 }
 
+
 /*
 void powNxN (unsigned (*A)[N], unsigned k, unsigned m) {
-  rem(A, m)
-  mat prod = Id();
+  unsigned mult = n%m;
+  unsigned prod = 1;
   while (k > 0) {
-    if ((k % 2) == 1) {
-      prod = (prod * A);
-      rem(A, m);
-    }
-    A = (A * A);
-    rem(A, m);
+    if ((k % 2) == 1)
+      prod = (prod * mult) % m;
+    mult = (mult * mult) % m;
     k /= 2;
   }
 }
 */
-void callmat() {
-  mat_t M;
-  mat_t A = Id();
-  mat_t B = Id();
-  M = matmul(A, B);
+void matcopy(mat_t from, mat_t to) {
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < N; ++j)
+      to[i][j] = from[i][j];
+}
 
+void powNxN (unsigned (*A)[N], unsigned k, unsigned m) {
+  mat_t tmp, prod;
+  rem(A, m);
+  prod = Id();
+  while (k > 0) {
+    if ((k % 2) == 1) {
+      tmp = matmul(prod, A);
+      matcopy(tmp, prod);
+      Mfree(tmp);
+      rem(prod, m);
+    }
+    tmp = matmul(A, A);
+    matcopy(tmp, A);
+    Mfree(tmp);
+    rem(A, m);
+    k /= 2;
+  }
+}
+
+void callmat() {
+  mat_t M = Id();
+  powNxN(M, 10, 4);
   Mprintf(M);
   Mfree(M);
-  Mfree(A);
-  Mfree(B);
 }

a4bda79a6e49edab1fe50976c894b0298d64b360
ENTER YOUR NAME <Enter_your@email.com>
2024-03-17 17:44:51 +0100

    malloc etc

--- 3HW/px/px.h
+++ 3HW/px/px.h
@@ -3,7 +3,13 @@
 
 
 #define N 10
+typedef unsigned row_t[N];
 void powNxN (unsigned (*A)[N], unsigned k, unsigned m);
 
 
+
+row_t ** idMat();
+void Mfree(row_t ** M);
+
+void Mprintf(row_t **M);
 #endif

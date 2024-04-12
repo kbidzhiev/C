f09de5fead3dd3089a7c0a80e665bd0d63c5a688
ENTER YOUR NAME <Enter_your@email.com>
2024-04-12 22:01:54 +0200

    build

--- 4HW/lr/main.c
+++ 4HW/lr/main.c
@@ -1,14 +1,19 @@
 #include "lr.h"
 
 
-struct node_t * build_list(FILE *f);
+struct node_t * build_list(FILE *f) {
+  struct node_t *top = calloc(1, sizeof(struct node_t));
+  struct node_t *list = top;
+  while(1 == fscanf(f, "%d", &(top -> data))) {
+    top -> next = calloc(1, sizeof(struct node_t));
+    top = top -> next;
+  }
+  return list;
+}
 
 int main(int argc, char **argv) {
   int i = 0; 
   FILE *f = fopen(argv[1], "r");
-  while(1 == fscanf(f, "%d", &i)) {
-    printf("%d ", i) ;
-  }
   fclose(f);
   return 0;
 }

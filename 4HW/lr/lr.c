037a481873f59cf06595b47add2d4ff4e8408b4e
ENTER YOUR NAME <Enter_your@email.com>
2024-04-12 22:36:37 +0200

    iterative reverse list

--- 4HW/lr/lr.c
+++ 4HW/lr/lr.c
@@ -2,6 +2,20 @@
 
 
 struct node_t *reverse(struct node_t *top) {
-  struct node_t * tmp;
+  struct node_t * i, *i_n, *i_p;
+  if (NULL == top) return NULL;
+  if (NULL == top->next) return top;
+  i = top;
+  i_n = top -> next;
+  i_p = NULL; 
+  while (NULL != i_n) {
+    i -> next = i_p; 
+    
+    i_p = i;
+    i = i_n;
+    i_n = i_n -> next;
+  }
+  i -> next = i_p; 
+  return i;
 
 }

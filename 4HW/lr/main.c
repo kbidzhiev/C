869a310e98f781c16292f3c1490aa46d1a3fd7be
ENTER YOUR NAME <Enter_your@email.com>
2024-04-12 22:17:50 +0200

    build and print

--- 4HW/lr/main.c
+++ 4HW/lr/main.c
@@ -2,18 +2,38 @@
 
 
 struct node_t * build_list(FILE *f) {
-  struct node_t *top = calloc(1, sizeof(struct node_t));
-  struct node_t *list = top;
-  while(1 == fscanf(f, "%d", &(top -> data))) {
+  struct node_t *top = NULL;
+  struct node_t *list = NULL;
+  int data = -1;
+  while(1 ==  fscanf(f, "%d", &data)){
+    if(NULL == list) {
+      top = calloc(1, sizeof(struct node_t));
+      top -> data = data;
+      list = top;
+      continue;
+    }
     top -> next = calloc(1, sizeof(struct node_t));
     top = top -> next;
+    top -> data = data;
   }
   return list;
 }
 
+void print_list(const struct node_t * top) {
+  while(NULL != top) {
+    printf("%d ", top -> data);
+    top = top -> next;
+  }
+  printf("\n");
+}
+
 int main(int argc, char **argv) {
-  int i = 0; 
+  struct node_t *top;
   FILE *f = fopen(argv[1], "r");
+
+  top = build_list(f);
+  print_list(top);
+
   fclose(f);
   return 0;
 }
